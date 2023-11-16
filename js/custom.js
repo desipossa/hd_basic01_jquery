dd8c0f9d40d94ef994c27b5975d28687f9a138f3
desipossa <c1jang@hanmail.net>
2023-11-16 13:02:47 +0900

    너무힘들다.

--- js/custom.js
+++ js/custom.js
@@ -69,5 +69,35 @@ $(function () {
         variableWidth: true,
     })
 
+    $('.mainSolution .solution_slide').on('afterChange', function (e, s, c) {
+        //c = 0,1,2....8
+        $('.mainSolution .solution_txt .txt_box li').removeClass('on');
+        $('.mainSolution .solution_txt .txt_box li').eq(c).addClass('on');
+        $('.mainSolution .solution_idx strong').text('0' + (c + 1));
+    });
 
+    $('#promotion').YTPlayer({
+        videoURL: 'https://youtu.be/5nnsleIF6Yw',
+        containment: '.mainPromotion .movie_case',
+        showControls: false,
+        autoPlay: false,
+        optimizeDisplay: false,
+        playOnlyIfVisible: true,
+    });
+
+
+    let movie = true;
+
+    $('.mainPromotion .movie_play').on('click', function () {
+        if (movie) {
+            $('#promotion').YTPPlay();
+        } else {
+            $('#promotion').YTPPause();
+        }
+
+        $(this).toggleClass('on');
+
+        movie = !movie;
+
+    });
 });
--- js/kcc2.js
+++ js/kcc2.js
@@ -3,11 +3,12 @@ $(function () {
 
     $('.mainVisual .visual_slide').on('init', function () {
         const current = $('.mainVisual .visual_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
 
+        const a = new Array(10);
 
-        const a = new Array(6);
         for (let i = 0; i < a.length; i++) {
-            current.find('.slide').append('<div class="s"><span class="bg"></span></div>')
+            current.find('.slide').append('<div class="s"><span class="bg"></span></div>');
         }
 
         const span = $('.slide .bg');
@@ -18,7 +19,7 @@ $(function () {
         })
 
 
-        current.addClass('on').siblings().removeClass('on');
+
 
     });
 
@@ -32,35 +33,29 @@ $(function () {
 
 
     $('.mainVisual .visual_slide').on('beforeChange', function () {
-        $('.slick-slide').find('.slide .bg').css({ width: 0 });
         $('.slick-slide').find('.slide').children().remove();
     })
     $('.mainVisual .visual_slide').on('afterChange', function (e, s, c) {
-        //console.log(e, s, c); // c=0,1
+
         $('.mainVisual .right_dots li').removeClass('on');
         $('.mainVisual .right_dots li').eq(c).addClass('on');
         $('.mainVisual .num span').text('0' + (c + 1));
 
         const current = $('.mainVisual .visual_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
 
-
-        const a = new Array(6);
+        const a = new Array(10);
 
         for (let i = 0; i < a.length; i++) {
-            current.find('.slide')
-                .append('<div class="s"><span class="bg"></span></div>')
+            current.find('.slide').append('<div class="s"><span class="bg"></span></div>');
         }
 
-        const span = $('.slide .bg');
+        const span = $('.slick-current .slide .bg');
         span.each(function (idx, itm) {
             $(itm).css({
                 background: `url(../images/mainVisual0${c + 1}.jpg) no-repeat calc((-100vw / ${a.length} * ${idx})) center`
             }).animate({ width: '100%' })
-        });
-
-        current.addClass('on').siblings().removeClass('on');
-
-
+        })
     });
 
     $('.mainVisual .right_dots li a').on('click', function (e) {
@@ -70,7 +65,7 @@ $(function () {
     });
 
 
-    let st = false;
+    let st = true;
     $('.mainVisual .play').on('click', function () {
         $(this).toggleClass('on');
 
